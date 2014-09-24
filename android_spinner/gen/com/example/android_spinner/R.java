1fa48264090a45db915cc7bcf4154ac1eec9f221
zzcbyl <cn36312@126.com>
2014-09-24 11:45:29 +0800

    update

--- android_spinner/gen/com/example/android_spinner/R.java
+++ android_spinner/gen/com/example/android_spinner/R.java
@@ -1103,6 +1103,7 @@ containing a value of this type.
         public static final int listMode=0x7f050001;
         public static final int list_item=0x7f050029;
         public static final int middle=0x7f050012;
+        public static final int myTextView=0x7f05003c;
         public static final int never=0x7f050009;
         public static final int none=0x7f050010;
         public static final int normal=0x7f050000;
--- android_spinner/src/com/example/android_spinner/MainActivity.java
+++ android_spinner/src/com/example/android_spinner/MainActivity.java
@@ -4,12 +4,17 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+	private TextView mytv;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+
+		mytv = (TextView) findViewById(R.id.myTextView);
+	}
 }
